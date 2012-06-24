73a34732e03245be2c016d6c8c5213657a69350f
hathanhthai <hathanhthai@9e9908f6-2a66-15b1-5ab6-67cc902a9d75>
2012-06-24 00:32:50 +0000

    Supported Spring repository which is web based Artifactory site without service API using Windows IE control, update attacher to always modified source path even if the source path is read only to support Android devs

--- trunk/org.freejava.javasourceattacher/src/org/freejava/tools/handlers/WebBasedArtifactorySourceCodeFinder.js
+++ trunk/org.freejava.javasourceattacher/src/org/freejava/tools/handlers/WebBasedArtifactorySourceCodeFinder.js
@@ -3,8 +3,8 @@ var gavsearchUrl = WScript.Arguments(1) // http://repo.springsource.org/webapp/g
 var filesha1 = WScript.Arguments(2) // 2bf96b7aa8b611c177d329452af1dc933e14501c
 
 var ie = WScript.CreateObject("InternetExplorer.Application")
-ie.Visible = true
-ie.Silent = false
+ie.Visible = false
+ie.Silent = true
 
 ie.Navigate(checksumsearchUrl)
 while (ie.Busy) WScript.Sleep(200)
@@ -21,31 +21,33 @@ var links = ie.document.getElementsByTagName('a')
 for (var i = 0; i < links.length; i++) {
   if (links.item(i).href.indexOf('.jar') == links.item(i).href.length - 4) link = links.item(i).href
 }
-var pom = link.substring(0, link.length - 4) + '.pom'
+if (link != '') {
+	var pom = link.substring(0, link.length - 4) + '.pom'
 
-var xmlDoc = WScript.CreateObject("Microsoft.XMLDOM")
-xmlDoc.async = false
-xmlDoc.load (pom)
-var groupId = xmlDoc.selectNodes('/project/groupId').item(0).text || xmlDoc.selectNodes('/project/parent/groupId').item(0).text
-var artifactId = xmlDoc.selectNodes('/project/artifactId').item(0).text
-var version = xmlDoc.selectNodes('/project/version').item(0).text || xmlDoc.selectNodes('/project/parent/version').item(0).text
+	var xmlDoc = WScript.CreateObject("Microsoft.XMLDOM")
+	xmlDoc.async = false
+	xmlDoc.load (pom)
+	var groupId = xmlDoc.selectNodes('/project/groupId').item(0).text || xmlDoc.selectNodes('/project/parent/groupId').item(0).text
+	var artifactId = xmlDoc.selectNodes('/project/artifactId').item(0).text
+	var version = xmlDoc.selectNodes('/project/version').item(0).text || xmlDoc.selectNodes('/project/parent/version').item(0).text
 
-ie.Navigate(gavsearchUrl)
-while (ie.Busy) WScript.Sleep(200)
+	ie.Navigate(gavsearchUrl)
+	while (ie.Busy) WScript.Sleep(200)
 
-ie.document.getElementsByName('groupIdField').item(0) = groupId
-ie.document.getElementsByName('artifactIdField').item(0) = artifactId
-ie.document.getElementsByName('versionField').item(0) = version
-ie.document.getElementsByName('classifierField').item(0) = 'sources'
-ie.document.getElementsByName('classifierField').item(0).form.item(':submit').item(0).click()
+	ie.document.getElementsByName('groupIdField').item(0) = groupId
+	ie.document.getElementsByName('artifactIdField').item(0) = artifactId
+	ie.document.getElementsByName('versionField').item(0) = version
+	ie.document.getElementsByName('classifierField').item(0) = 'sources'
+	ie.document.getElementsByName('classifierField').item(0).form.item(':submit').item(0).click()
 
-while (ie.Busy || ie.document.getElementsByTagName('body').item(0).innerText.indexOf('matches') == -1) WScript.Sleep(200)
+	while (ie.Busy || ie.document.getElementsByTagName('body').item(0).innerText.indexOf('matches') == -1) WScript.Sleep(200)
 
-link = ''
-links = ie.document.getElementsByTagName('a')
-for (var i = 0; i < links.length; i++) {
-  if (links.item(i).href.indexOf('-sources.jar') == links.item(i).href.length - 12) link = links.item(i).href
+	link = ''
+	links = ie.document.getElementsByTagName('a')
+	for (var i = 0; i < links.length; i++) {
+	  if (links.item(i).href.indexOf('-sources.jar') == links.item(i).href.length - 12) link = links.item(i).href
+	}
+
+	WScript.Echo(link)
 }
-WScript.Echo(link)
-
 ie.Quit()
