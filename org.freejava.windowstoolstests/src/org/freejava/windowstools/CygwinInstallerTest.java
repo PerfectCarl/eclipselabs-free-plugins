b4481efb8c1caab41472fd0078a8546ed909ae88
hathanhthai@gmail.com <hathanhthai@gmail.com@9e9908f6-2a66-15b1-5ab6-67cc902a9d75>
2014-02-03 14:23:42 +0000

    updated windowstools to support cygwin (new)

--- org.freejava.windowstoolstests/src/org/freejava/windowstools/CygwinInstallerTest.java
+++ org.freejava.windowstoolstests/src/org/freejava/windowstools/CygwinInstallerTest.java
@@ -1,15 +1,13 @@
 package org.freejava.windowstools;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class CygwinInstallerTest {
 
-	@Test
-	public void testInstall() {
-		CygwinInstaller obj = new CygwinInstaller();
-		obj.install("C:\1");
-	}
+    @Test
+    public void testInstall() {
+        CygwinInstaller obj = new CygwinInstaller();
+        obj.install("C:\\1");
+    }
 
 }
