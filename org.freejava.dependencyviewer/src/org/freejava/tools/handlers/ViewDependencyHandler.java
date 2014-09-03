722d78268d758fa09402ca73c0e81cd576fd0aee
hathanhthai <hathanhthai@9e9908f6-2a66-15b1-5ab6-67cc902a9d75>
2014-09-03 10:18:52 +0000

    Code cleanup

--- org.freejava.dependencyviewer/src/org/freejava/tools/handlers/ViewDependencyHandler.java
+++ org.freejava.dependencyviewer/src/org/freejava/tools/handlers/ViewDependencyHandler.java
@@ -1,7 +1,6 @@
 package org.freejava.tools.handlers;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -31,24 +30,7 @@ import org.eclipse.ui.handlers.HandlerUtil;
 import org.freejava.tools.Activator;
 import org.freejava.tools.handlers.dependency.DependencyView;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Splitter;
-import com.jeantessier.classreader.AggregatingClassfileLoader;
-import com.jeantessier.classreader.ClassNameHelper;
-import com.jeantessier.classreader.ClassfileLoader;
-import com.jeantessier.classreader.Field_info;
-import com.jeantessier.classreader.LoadListenerVisitorAdapter;
-import com.jeantessier.classreader.Method_info;
-import com.jeantessier.classreader.Signature_attribute;
-import com.jeantessier.classreader.TransientClassfileLoader;
-import com.jeantessier.classreader.Visitable;
-import com.jeantessier.classreader.Visitor;
-import com.jeantessier.dependency.CodeDependencyCollector;
-import com.jeantessier.dependency.CollectionSelectionCriteria;
-import com.jeantessier.dependency.GraphSummarizer;
-import com.jeantessier.dependency.LinkMaximizer;
 import com.jeantessier.dependency.Node;
-import com.jeantessier.dependency.NodeFactory;
 
 /**
  * Handler class for View Class/Package Dependency action.
@@ -87,9 +69,9 @@ public class ViewDependencyHandler extends AbstractHandler {
 
             Collection<Node> graphNodes;
             if (isViewPackageDependency) {
-                graphNodes = getPackageDependency(files, names);
+                graphNodes = new DependencyFinder().getPackageDependency(files, names);
             } else {
-                graphNodes = getClassDependency(files, names);
+                graphNodes = new DependencyFinder().getClassDependency(files, names);
             }
 
             IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
@@ -328,89 +310,4 @@ public class ViewDependencyHandler extends AbstractHandler {
         }
     }
 
-    private Collection<Node> getPackageDependency(Collection<File> files, Collection<String> names) {
-
-        Collection<String> sources = new HashSet<String>();
-        for (File file : files) {
-            sources.add(file.getAbsolutePath());
-        }
-
-        NodeFactory factory = new NodeFactory();
-        Visitor visitor = new CodeDependencyCollector(factory);
-        ClassfileLoader loader = new TransientClassfileLoader();
-        loader.addLoadListener(new LoadListenerVisitorAdapter(visitor));
-        loader.load(sources);
-
-        LinkMaximizer maximizer = new LinkMaximizer();
-        maximizer.traverseNodes(factory.getPackages().values());
-
-        CollectionSelectionCriteria scopeCriteria = new CollectionSelectionCriteria(names, null);
-        scopeCriteria.setMatchingPackages(true);
-        scopeCriteria.setMatchingClasses(false);
-        scopeCriteria.setMatchingFeatures(false);
-        CollectionSelectionCriteria filterCriteria = new CollectionSelectionCriteria(names, null);
-        filterCriteria.setMatchingPackages(true);
-        filterCriteria.setMatchingClasses(false);
-        filterCriteria.setMatchingFeatures(false);
-
-        GraphSummarizer dependenciesQuery = new GraphSummarizer(scopeCriteria, filterCriteria);
-        dependenciesQuery.traverseNodes(factory.getPackages().values());
-        Collection<Node> nodes = new ArrayList<Node>();
-        nodes.addAll(dependenciesQuery.getScopeFactory().getPackages().values());
-        return nodes;
-    }
-
-    public Collection<Node> getClassDependency(Collection<File> files, Collection<String> names) {
-        Collection<String> sources = new HashSet<String>();
-        for (File file : files) {
-            sources.add(file.getAbsolutePath());
-        }
-
-        NodeFactory factory = new NodeFactory();
-        Visitor visitor = new CodeDependencyCollector(factory) {
-        	@Override
-        	public void visitSignature_attribute(Signature_attribute attribute) {
-        		super.visitSignature_attribute(attribute);
-        		Visitable owner = attribute.getOwner();
-        		String ownerFullSignature = null;
-        		if (owner instanceof Field_info) {
-        			ownerFullSignature = ((Field_info) owner).getFullSignature();
-        		} else if (owner instanceof Method_info) {
-        			ownerFullSignature = ((Method_info) owner).getFullSignature();
-        		}
-        		if (ownerFullSignature != null) {
-	        		String sig = attribute.getSignature();
-	        		for (String id : Splitter.on(CharMatcher.anyOf("<>;")).omitEmptyStrings().split(sig)) {
-	                	if (id.startsWith("L")) {
-	                		String className = id.substring(1);
-	                		className = ClassNameHelper.path2ClassName(className);
-	                		Node node = getFactory().createFeature(ownerFullSignature, true);
-	                		node.addDependency(getFactory().createClass(className));
-	                	}
-	        		}
-        		}
-        	}
-        };
-        ClassfileLoader loader = new AggregatingClassfileLoader();
-        loader.addLoadListener(new LoadListenerVisitorAdapter(visitor));
-        loader.load(sources);
-
-        LinkMaximizer maximizer = new LinkMaximizer();
-        maximizer.traverseNodes(factory.getPackages().values());
-
-        CollectionSelectionCriteria scopeCriteria = new CollectionSelectionCriteria(names, null);
-        scopeCriteria.setMatchingPackages(false);
-        scopeCriteria.setMatchingClasses(true);
-        scopeCriteria.setMatchingFeatures(false);
-        CollectionSelectionCriteria filterCriteria = new CollectionSelectionCriteria(names, null);
-        filterCriteria.setMatchingPackages(false);
-        filterCriteria.setMatchingClasses(true);
-        filterCriteria.setMatchingFeatures(false);
-
-        GraphSummarizer dependenciesQuery = new GraphSummarizer(scopeCriteria, filterCriteria);
-        dependenciesQuery.traverseNodes(factory.getPackages().values());
-        Collection<Node> nodes = new ArrayList<Node>();
-        nodes.addAll(dependenciesQuery.getScopeFactory().getClasses().values());
-        return nodes;
-    }
 }
